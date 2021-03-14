4f0d2d22e259b3c898e56a59f3863cbaceaac2bb
Gábor Sápi <gaborsapi9999@gmail.com>
2021-03-14 11:49:47 +0100

    2nd excercise added to Homework1.java

--- src/main/java/countries/Homework1.java
+++ src/main/java/countries/Homework1.java
@@ -27,8 +27,7 @@ public class Homework1 {
      *  Returns the first country name that contains the word "island" ignoring case.
      */
     public Optional<String> streamPipeline2() {
-        // TODO
-        return null;
+        return countries.stream().map(c -> c.getName()).filter(c -> c.toLowerCase().contains("island")).findFirst();
     }
 
     /**
